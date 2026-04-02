d8c67d7e87c8691405a11d95edca7615ac4fdee1
Rubén Luengo <rubluesan2@alu.edu.gva.es>
2026-04-02 12:55:04 +0200

    refactor: reestructuracion de carpetas del proyecto + componentes pages generados

---
+++ src/app/pages/disposition/disposition.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-disposition',
+  imports: [],
+  templateUrl: './disposition.html',
+  styleUrl: './disposition.css',
+})
+export class Disposition {
+
+}
