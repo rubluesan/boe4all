bcd39720ac351d36b955c1816a191a3688af9208
Rubén Luengo <rubluesan2@alu.edu.gva.es>
2026-04-09 16:08:00 +0200

    refactor: movidos guards a la carpeta correspondiente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,13 @@ import { Home } from './pages/home/home';
 import { Landing } from './pages/landing/landing';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
-import { guardedRoutesGuard } from './guarded-routes-guard';
+import { guardedRoutesGuard } from './core/guards/guarded-routes-guard';
 
 export const routes: Routes = [
-    { path: '', component: Landing },
-    { path: '*', component: Landing },
-    { path: 'landing', component: Landing },
-    { path: 'home', component: Home , canActivate: [guardedRoutesGuard]},
-    { path: 'login', component: Login },
-    { path: 'register', component: Register },
+  { path: '', component: Landing },
+  { path: '*', component: Landing },
+  { path: 'landing', component: Landing },
+  { path: 'home', component: Home, canActivate: [guardedRoutesGuard] },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
 ];
--- src/app/core/guards/guarded-routes-guard.ts
+++ src/app/core/guards/guarded-routes-guard.ts
@@ -1,6 +1,6 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { SupabaseService } from './core/services/supabase-service';
+import { SupabaseService } from '../services/supabase-service';
 
 export const guardedRoutesGuard: CanActivateFn = async (route, state) => {
   const router = inject(Router);
