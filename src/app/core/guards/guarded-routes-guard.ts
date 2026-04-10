0baafa9a7bbaaa791efbeeb1e882f38d55efa0d6
Rubén Luengo <rubluesan2@alu.edu.gva.es>
2026-04-10 16:21:44 +0200

    modified: añadido mensaje al guard para no autenticados

--- src/app/core/guards/guarded-routes-guard.ts
+++ src/app/core/guards/guarded-routes-guard.ts
@@ -1,16 +1,19 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { SupabaseService } from '../services/supabase-service';
+import { SystemMessageService } from '../services/system-message-service';
 
 export const guardedRoutesGuard: CanActivateFn = async (route, state) => {
   const router = inject(Router);
   const supabase = inject(SupabaseService);
+  const messageService = inject(SystemMessageService);
 
   const isAuthenticated = await supabase.authReady();
 
   if (isAuthenticated) {
     return true;
   } else {
+    messageService.showMessage('Debes iniciar sesión para acceder a la aplicación', true);
     return router.createUrlTree(['/login']);
   }
 };
