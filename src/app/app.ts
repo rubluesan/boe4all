c7cd4ae7bc4e6550af5ac8476ca53b5bd509837e
Enrique <enriquecastellon.pereboil@gmail.com>
2026-04-15 18:53:40 +0200

    Arreglos ruta breadcrumbs

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,9 +4,10 @@ import { NavbarShow } from './shared/components/navbar-show/navbar-show';
 import { Footer } from './shared/components/footer/footer';
 import { NotificationToast } from './shared/components/notification-toast/notification-toast';
 import { SystemMessageService } from './core/services/system-message-service';
+import { BreadcrumbComponent } from './shared/components/breadcrumbs/breadcrumbs';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarShow, Footer, NotificationToast],
+  imports: [RouterOutlet, NavbarShow, Footer, NotificationToast, BreadcrumbComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/shared/components/breadcrumbs/breadcrumbs.ts
+++ src/app/shared/components/breadcrumbs/breadcrumbs.ts
@@ -1,11 +1,13 @@
 // breadcrumb.component.ts
 import { Component } from '@angular/core';
 import { BreadcrumbService } from '../../../core/services/breadcrumbs';
-
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-breadcrumb',
-  templateUrl: './breadcrumb.component.html',
-  styleUrls: ['./breadcrumb.component.css']
+  templateUrl: './breadcrumbs.html',
+  styleUrls: ['./breadcrumbs.css'],
+  imports: [RouterLink, CommonModule],
 })
 export class BreadcrumbComponent {
   constructor(public breadcrumbService: BreadcrumbService) {}
