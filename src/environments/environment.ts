74ad29631d02064738099f97587afe3e9704af41
Enrique <enriquecastellon.pereboil@gmail.com>
2026-04-18 11:21:02 +0200

    url apiboe puesta en enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: false,
   supabaseUrl: 'https://bhabkiwaddorvgkukofo.supabase.co',
   supabaseKey: 'sb_publishable_VOuk993hfkklIkGoSdJwZw_Zic9lRqy',
+  boeapiUrl: 'http://boe.es/datosabiertos/api/boe/sumario/',
 };
