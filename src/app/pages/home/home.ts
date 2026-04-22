eea5adad33aef3af5ac3f523dfc054046643d84e
Rubén Luengo <rubluesan2@alu.edu.gva.es>
2026-04-22 13:51:34 +0200

    cambios para probar api boe

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { BoeService } from '../../core/services/boe-service';
+import { dateToYYYYMMDD } from '../../shared/utils/date-to-yyyymmdd';
 @Component({
   selector: 'app-home',
   imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit {
+  private boeService = inject(BoeService);
+
+  ngOnInit() {
+    this.getTodaysBoe();
+  }
+
+  getTodaysBoe() {
+    this.boeService.getDailySummary(dateToYYYYMMDD(new Date())).subscribe({
+      next: (response) => {
+        console.log('BOE data for today:', response.body);
+      },
+      error: (error) => {
+        console.error('Error fetching BOE data:', error);
+      },
+    });
+  }
+}
