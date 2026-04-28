51dccb94f1f08af0f3738a29f363c9df7fca7e3d
Enrique <enriquecastellon.pereboil@gmail.com>
2026-04-28 19:11:07 +0200

    evento submit creado y custom repetido fuera

--- src/app/pages/home/components/custom-searcher/custom-searcher.ts
+++ src/app/pages/home/components/custom-searcher/custom-searcher.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormField, form, min, max, required } from '@angular/forms/signals';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-custom-searcher',
-  imports: [FormField],
+  imports: [FormField, Router],
   templateUrl: './custom-searcher.html',
   styleUrl: './custom-searcher.css',
 })
@@ -12,6 +13,7 @@ export class CustomSearcher {
     month: '',
     year: '',
   });
+  ruta = inject(Router);
   formulario = form(this.data, (schemaPath) => {
     required(schemaPath.day, {
       message: 'Debes introducir un día para realizar la búsqueda.',
@@ -41,4 +43,19 @@ export class CustomSearcher {
       message: 'El año debe ser un número entre 1900 y ' + new Date().getFullYear() + '.',
     });
   });
+
+  onSubmit() {
+  const day = this.formulario.day;
+  const month = this.formulario.month;
+  const year = this.formulario.year;
+
+  // aseguramos formato de 2 dígitos
+  const dia = String(day).padStart(2, '0');
+  const mes = String(month).padStart(2, '0');
+
+  const fechaboe = `${year}${mes}${dia}`;
+  this.ruta.navigate(['/search', fechaboe]);//ruta por defecto ya cambiare el nombre así hay algo ahí
+  console.log(`Buscando eventos para la fecha: ${fechaboe}`);
+  
+  }
 }
