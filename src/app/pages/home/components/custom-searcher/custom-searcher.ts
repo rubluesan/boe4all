40fb4c6822def09506b96db9627d883fbdc79ed2
Rubén Luengo <rubluesan2@alu.edu.gva.es>
2026-04-28 10:38:04 +0200

    add: validaciones a componente busqueda por fecha

--- src/app/pages/home/components/custom-searcher/custom-searcher.ts
+++ src/app/pages/home/components/custom-searcher/custom-searcher.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { FormField, form } from '@angular/forms/signals';
+import { FormField, form, min, max, required } from '@angular/forms/signals';
 @Component({
   selector: 'app-custom-searcher',
   imports: [FormField],
@@ -13,8 +13,32 @@ export class CustomSearcher {
     year: '',
   });
   formulario = form(this.data, (schemaPath) => {
-    Number(schemaPath.day);
-    Number(schemaPath.month);
-    Number(schemaPath.year);
+    required(schemaPath.day, {
+      message: 'Debes introducir un día para realizar la búsqueda.',
+    });
+    min(schemaPath.day, 1, {
+      message: 'El día debe ser un número entre 1 y 31.',
+    });
+    max(schemaPath.day, 31, {
+      message: 'El día debe ser un número entre 1 y 31.',
+    });
+    required(schemaPath.month, {
+      message: 'Debes introducir un mes para realizar la búsqueda.',
+    });
+    min(schemaPath.month, 1, {
+      message: 'El mes debe ser un número entre 1 y 12.',
+    });
+    max(schemaPath.month, 12, {
+      message: 'El mes debe ser un número entre 1 y 12.',
+    });
+    required(schemaPath.year, {
+      message: 'Debes introducir un año para realizar la búsqueda.',
+    });
+    min(schemaPath.year, 1900, {
+      message: 'El año debe ser un número entre 1900 y ' + new Date().getFullYear() + '.',
+    });
+    max(schemaPath.year, new Date().getFullYear(), {
+      message: 'El año debe ser un número entre 1900 y ' + new Date().getFullYear() + '.',
+    });
   });
 }
