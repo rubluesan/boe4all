45ead4fbd7eecbe5c9174147b00f16338e912a59
Enrique <enriquecastellon.pereboil@gmail.com>
2026-05-12 18:49:45 +0200

    Sumario tiene la fecha y service comentado eso para reciclaje

--- src/app/pages/summary/summary.ts
+++ src/app/pages/summary/summary.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
-
+import { inject} from '@angular/core';
+import {BoeService} from '../../core/services/boe-service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-summary',
   imports: [],
   templateUrl: './summary.html',
   styleUrl: './summary.css',
 })
-export class Summary {}
+export class Summary {
+  private service = inject(BoeService);
+  route = inject(ActivatedRoute);
+   ngOnInit() {
+  const fecha = this.route.snapshot.paramMap.get('fecha');
+  console.log(fecha);
+  this.service.getDailySummary(fecha).subscribe(response => {
+    console.log(response.body);
+  });
+}
+}
