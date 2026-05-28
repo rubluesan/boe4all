d89c7cdc696168e3a8a6221d22484df79ef1d65e
Rubén Luengo <rubluesan2@alu.edu.gva.es>
2026-05-28 17:32:36 +0200

    añadidas funciones para ir al boe siguiente y anterior

--- src/app/core/services/boe-service.ts
+++ src/app/core/services/boe-service.ts
@@ -1,19 +1,57 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { BoeDataResponse, BoeSumario } from '../models/BoeData';
-import { ActivatedRoute } from '@angular/router';
+import {
+  dateToString,
+  obtenerDiaAnterior,
+  obtenerDiaSiguiente,
+} from '../../shared/utils/fechas-boe';
+
 @Injectable({
   providedIn: 'root',
 })
 export class BoeService {
   private http = inject(HttpClient);
   private apiUrl = environment.boeapiUrl;
-  public getDailySummary(date: string |null): Observable<HttpResponse<BoeDataResponse>> {
+
+  private readonly FECHA_MINIMA = '19600901'; // 1 de Septiembre de 1960 - primera publicación del BOE moderno
+
+  public getDailySummary(date: string | null): Observable<HttpResponse<BoeDataResponse>> {
     return this.http.get<BoeDataResponse>(this.apiUrl + `/${date}`, {
       observe: 'response',
     });
   }
-  
+
+  // Buscar hacia atrás (BOE Anterior)
+  buscarBoeAnterior(fechaActual: string): Observable<BoeDataResponse> {
+    const fechaClave = obtenerDiaAnterior(fechaActual);
+
+    if (parseInt(fechaClave, 10) < parseInt(this.FECHA_MINIMA, 10)) {
+      return throwError(() => new Error('Has llegado al primer BOE de la historia.'));
+    }
+
+    return this.http.get<BoeDataResponse>(this.apiUrl + `/${fechaClave}`).pipe(
+      catchError((error) => {
+        return this.buscarBoeAnterior(fechaClave);
+      }),
+    );
+  }
+
+  // Buscar hacia adelante (BOE Siguiente)
+  buscarBoeSiguiente(fechaActual: string): Observable<BoeDataResponse> {
+    const fechaClave = obtenerDiaSiguiente(fechaActual);
+    const hoyStr = dateToString(new Date());
+
+    if (parseInt(fechaClave, 10) > parseInt(hoyStr, 10)) {
+      return throwError(() => new Error('No hay publicaciones futuras disponibles.'));
+    }
+
+    return this.http.get<BoeDataResponse>(this.apiUrl + `/${fechaClave}`).pipe(
+      catchError((error) => {
+        return this.buscarBoeSiguiente(fechaClave);
+      }),
+    );
+  }
 }
