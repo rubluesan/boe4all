cf3864cd0505c9c0ec9253de4aa7ad9b0748bbfa
Rubén Luengo <rubluesan2@alu.edu.gva.es>
2026-05-29 14:12:30 +0200

    arreglos selector fecha del Home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,18 +1,17 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { BoeService } from '../../core/services/boe-service';
-import { CustomSearcher } from './components/custom-searcher/custom-searcher';
-import { form, FormField, required } from '@angular/forms/signals';
 import { dateToString } from '../../shared/utils/fechas-boe';
 @Component({
   selector: 'app-home',
-  imports: [FormField],
+  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home implements OnInit {
   private boeService = inject(BoeService);
   private router = inject(Router);
+  todaysDate = new Date().toISOString().split('T')[0]; // YYYY-MM-DD para el input date
 
   ngOnInit() {
     this.getTodaysBoe();
@@ -29,21 +28,8 @@ export class Home implements OnInit {
     });
   }
 
-  formModel = signal({
-    date: '',
-  });
-
-  dateSelectForm = form(this.formModel, (schemaPath) => {
-    required(schemaPath.date, {
-      message: 'Debes introducir una fecha para realizar la búsqueda.',
-    });
-  });
-
-  async onDateSubmit() {
-    const fechaString = this.dateSelectForm().value().date;
-
+  async onDateSubmit(fechaString: string) {
     if (!fechaString) return;
-    console.log('Fecha seleccionada:', fechaString);
 
     // Transformamos "2026-05-27" en "20260527" para el BOE quitando los guiones
     const fechaboe = fechaString.replace(/-/g, '');
